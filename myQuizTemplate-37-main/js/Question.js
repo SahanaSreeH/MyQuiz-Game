db0e44e4594c342ec4aeacb80600250057e6b8e7
SahanaSreeH <77058482+SahanaSreeH@users.noreply.github.com>
2021-04-30 20:54:27 +0530

    Add files via upload

---
+++ myQuizTemplate-37-main/js/Question.js
@@ -0,0 +1,27 @@
+class Question{
+    constructor(){
+        this.input1 = createInput("Name");
+        this.title = createElement("h2");
+        this.question = createElement("h2");
+        this.option1 = createElement("h2");
+        this.option2 = createElement("h2");
+    }
+
+    hide(){
+
+    }
+
+    display(){
+        this.title.html("MyQuiz Game");
+        this.title.position(350, 0);
+
+        this.question.html("Question:- What starts and ends with the letter 'E', but has only one letter? ");
+        this.question.position(150, 80);
+        this.option1.html("1: Everyone " );
+        this.option1.position(150, 100);
+        this.option2.html("2; Envelope " );
+        this.option2.position(150, 120);
+
+        this.input1.position(150, 230);
+    }
+}
